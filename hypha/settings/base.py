e1e27db3c87077d287f049f2372bfd293fdfee9f
Chris Zubak-Skees <chriszs@gmail.com>
2021-09-22 22:30:47 -0400

    Custom workflow state names
    Adds a new English locale for ARDC-specific translations

--- hypha/settings/base.py
+++ hypha/settings/base.py
@@ -326,7 +326,7 @@ PASSWORD_RESET_TIMEOUT_DAYS = 8
 
 # Language code in standard language id format: en, en-gb, en-us
 # The corrosponding locale dir is named: en, en_GB, en_US
-LANGUAGE_CODE = env.get('LANGUAGE_CODE', 'en')
+LANGUAGE_CODE = env.get('LANGUAGE_CODE', 'en_ARDC')
 
 CURRENCY_SYMBOL = env.get('CURRENCY_SYMBOL', '$')
 
