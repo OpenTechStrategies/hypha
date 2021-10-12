d46fbfe06ac695f465ba89e7e7641a6a228d192b
Chris Zubak-Skees <chriszs@gmail.com>
2021-10-12 00:56:29 -0400

    Switch the default locale en-us

--- hypha/settings/base.py
+++ hypha/settings/base.py
@@ -299,7 +299,7 @@ PASSWORD_PAGE_TIMEOUT_SECONDS = env.int('PASSWORD_PAGE_TIMEOUT_SECONDS', 120)
 
 # Language code in standard language id format: en, en-gb, en-us
 # The corrosponding locale dir is named: en, en_GB, en_US
-LANGUAGE_CODE = env.str('LANGUAGE_CODE', 'en')
+LANGUAGE_CODE = env.str('LANGUAGE_CODE', 'en-us')
 
 CURRENCY_SYMBOL = env.str('CURRENCY_SYMBOL', '$')
 
