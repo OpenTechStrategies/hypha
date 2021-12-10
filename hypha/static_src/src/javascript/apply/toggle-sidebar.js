0a49575a2af2b955cb912b53c5bbe59c8b0e22ee
Chris Zubak-Skees <chriszs@gmail.com>
2021-12-10 17:32:05 -0500

    WIP

--- hypha/static_src/src/javascript/apply/toggle-sidebar.js
+++ hypha/static_src/src/javascript/apply/toggle-sidebar.js
@@ -2,12 +2,44 @@
 
     'use strict';
 
+    function getSidebarToggle() {
+        return $('.js-sidebar-toggle');
+    }
+
+    function getCurrentTab() {
+        return $('.tabs__content--current');
+    }
+
+    function containsSidebar($tab) {
+        return $tab.find('.sidebar').length > 0;
+    }
+
+    function showToggle($sidebarToggle) {
+        return $sidebarToggle.css('visibility', 'visible');
+    }
+
+    function hideToggle($sidebarToggle) {
+        return $sidebarToggle.css('visibility', 'hidden');
+    }
+
+    function showOrHideSidebarToggle() {
+        var $sidebarToggle = getSidebarToggle();
+        if (containsSidebar(getCurrentTab())) {
+            return showToggle($sidebarToggle);
+        }
+        else {
+            return hideToggle($sidebarToggle);
+        }
+    }
+
     var $sidebar = $('.sidebar');
     $('.tabs__container').append('<a class="tab__item tab__item--right js-sidebar-toggle" href="#">Toggle sidebar</a>');
     $('.js-sidebar-toggle').click(function (e) {
         e.preventDefault();
         $sidebar.toggleClass('hidden');
     });
+    $(window).on('hashchange', showOrHideSidebarToggle);
+    showOrHideSidebarToggle();
 
 })(jQuery);
 
