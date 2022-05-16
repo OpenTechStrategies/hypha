69e0d346f78b643ae04f06b77dee9cf7702c0175
Chris Zubak-Skees <chriszs@gmail.com>
2022-05-16 13:57:35 -0400

    feat: default comment visibility to everyone Closes #100

--- hypha/apply/activity/models.py
+++ hypha/apply/activity/models.py
@@ -129,11 +129,11 @@ class Activity(models.Model):
     @classmethod
     def visibility_for(cls, user):
         if user.is_apply_staff:
-            return [TEAM, REVIEWER, PARTNER, ALL]
+            return [ALL, REVIEWER, PARTNER, TEAM]
         if user.is_reviewer:
-            return [REVIEWER, ALL]
+            return [ALL, REVIEWER]
         if user.is_partner:
-            return [PARTNER, ALL]
+            return [ALL, PARTNER]
         if user.is_applicant:
             return []
 
