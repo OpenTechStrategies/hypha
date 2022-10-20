2b213f374787e26bd89568924e52d7db8e6c56f2
Ghulam Murtaza <gmurtaza@opentechstrategies.com>
2022-10-20 12:21:20 -0400

    Export applications in csv by round

--- hypha/apply/funds/views.py
+++ hypha/apply/funds/views.py
@@ -1,6 +1,7 @@
 from copy import copy
 from datetime import timedelta
 from itertools import chain
+import os
 
 import django_tables2 as tables
 from django.conf import settings
@@ -11,7 +12,8 @@ from django.contrib.auth.mixins import UserPassesTestMixin
 from django.contrib.humanize.templatetags.humanize import intcomma
 from django.core.exceptions import PermissionDenied
 from django.db.models import Count, F, Q
-from django.http import FileResponse, Http404, HttpResponseRedirect
+from django.http import FileResponse, Http404, HttpResponseRedirect, HttpResponse
+from django.core.management import call_command
 from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
 from django.utils import timezone
@@ -535,6 +537,26 @@ class SubmissionUserFlaggedView(UserPassesTestMixin, BaseAdminSubmissionsTable):
     def test_func(self):
         return self.request.user.is_apply_staff or self.request.user.is_reviewer
 
+@method_decorator(staff_required, name='dispatch')
+class ExportSubmissionsByRound(BaseAdminSubmissionsTable):
+    
+    def get_queryset(self):
+        try:
+            self.obj = Page.objects.get(pk=self.kwargs.get('pk')).specific
+        except Page.DoesNotExist:
+            raise Http404(_("No Round or Lab found matching the query"))
+
+        if not isinstance(self.obj, (LabBase, RoundBase)):
+            raise Http404(_("No Round or Lab found matching the query"))
+        return super().get_queryset().filter(Q(round=self.obj) | Q(page=self.obj))
+
+    def get(self, request, pk):
+        self.get_queryset()        
+        file_path = call_command('export_submissions_by_round', [self.obj, pk])
+        with open(file_path, 'rb') as file:
+            response = HttpResponse(file.read(), content_type="text/csv")
+            response['Content-Disposition'] = 'inline; filename=' + os.path.basename(file_path)
+            return response
 
 @method_decorator(staff_required, name='dispatch')
 class SubmissionsByRound(BaseAdminSubmissionsTable, DelegateableListView):
