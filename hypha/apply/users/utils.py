ab21b025ba03c5a8d10b4cf82dc5804032df11f6
Ghulam Murtaza <gmurtaza@opentechstrategies.com>
2022-10-24 08:40:13 -0400

    Activation email subject template string changed to fix 500 error

--- hypha/apply/users/utils.py
+++ hypha/apply/users/utils.py
@@ -51,7 +51,7 @@ def send_activation_email(user, site=None):
     if site:
         context.update(site=site)
 
-    subject = 'Account details for {username} at {ORG_LONG_NAME}'.format(**context)
+    subject = 'Account details for {username} at {org_long_name}'.format(**context)
     # Force subject to a single line to avoid header-injection issues.
     subject = ''.join(subject.splitlines())
     message = render_to_string('users/activation/email.txt', context)
