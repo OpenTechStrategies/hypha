6e181fcc08efa8d5bc521c9ef1b277710777d2a6
Ghulam Murtaza <gmurtaza@opentechstrategies.com>
2022-11-15 16:00:46 -0500

    Removed unnecessary comments

--- hypha/apply/funds/forms.py
+++ hypha/apply/funds/forms.py
@@ -191,7 +191,6 @@ class BatchDeleteSubmissionForm(forms.Form):
 
 
 class UpdateReviewersForm(ApplicationSubmissionModelForm):
-    #External Reviewers
     reviewer_reviewers = forms.ModelMultipleChoiceField(
         queryset=User.objects.reviewers().only('pk', 'full_name'),
         widget=Select2MultiCheckboxesWidget(attrs={'data-placeholder': 'Reviewers'}),
