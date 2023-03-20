507b7c8c9181a5d26d80a62c0b187a00b58a86b2
Ghulam Murtaza <gmurtaza@opentechstrategies.com>
2023-03-20 11:20:56 -0400

    dictionary key corrected

--- hypha/apply/determinations/tests/test_admin_views.py
+++ hypha/apply/determinations/tests/test_admin_views.py
@@ -18,7 +18,7 @@ class TestCreateDeterminationFormView(TestCase):
         }
         cls.send_notice_data = {
             'field_label': factory.Faker('sentence').evaluate(None, None, {'locale': None}),
-            'help_text': factory.Faker('sentence').evaluate(None, None, {'locale': None}),
+            'field_help_text': factory.Faker('sentence').evaluate(None, None, {'locale': None}),
             'default_value': True,
         }
         cls.name = factory.Faker('name').evaluate(None, None, {'locale': None})
